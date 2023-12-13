87eefa0f82fe2ab3359ce2a5db40bbb5d4dfa7e0
Umang Bisht <umang.bisht@sveltetech.com>
2023-12-13 10:48:47 +0530

    conversion before indexing

--- json_file_search.py
+++ json_file_search.py
@@ -3,6 +3,10 @@ import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
+import csv
+from io import StringIO
+import pandas as pd
+from io import BytesIO
 # from elasticsearch.helpers import bulk
 
 app = Flask(__name__)
@@ -38,10 +42,11 @@ def get_search_data():
             "bool": {
                 "should": [
                     {
-                        "query_string": {
-                        "query": f"*{search_query}* OR {search_query}*",
-                        "fields": ["*"]
-                    }
+                        "multi_match": {
+                            "query": search_query,
+                            "type": "phrase",
+                        
+                        }
                         # "multi_match": {
                         #     "query": search_query,
                         #     "type": "phrase",
@@ -50,25 +55,36 @@ def get_search_data():
                     },
                   
                 ],
-                
-            },
-            "bool": {
-                "should": [
+                "must": [
                     {
-                    #     "query_string": {
-                    #     "query": f"*{search_query}* OR {search_query}*",
-                    #     "fields": ["*"]
-                    # }
-                        "multi_match": {
-                            "query": search_query,
-                            "type": "phrase",
-                        
-                        }
+                        "query_string": {
+                        "query": f"*{search_query}* OR {search_query}*",
+                        "fields": ["*"]
+                    }
+
                     },
                   
                 ],
                 
-            }
+                
+            },
+            # "bool": {
+            #     "must": [
+            #         {
+            #         #     "query_string": {
+            #         #     "query": f"*{search_query}* OR {search_query}*",
+            #         #     "fields": ["*"]
+            #         # }
+            #             "multi_match": {
+            #                 "query": search_query,
+            #                 "type": "phrase",
+                        
+            #             }
+            #         },
+                  
+            #     ],
+                
+            # }
         }
     }
 
@@ -91,22 +107,29 @@ def get_search_data():
     
 @app.route('/indexing-file', methods=['POST'])
 def make_file_index():
-    FILE_TO_CONVERT = ['csv','txt']
+    FILE_TO_CONVERT = ['csv','txt','xlsx']
     # Check if the post request has the file part
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
+    
+    
 
     file = request.files['file']
     filename = file.filename.lower()
-    filename = filename.replace(" ", "")
-    # Split from the last dot
-    split_result = filename.rsplit('.', 1)
-
-    print("split_result", split_result)
-
     # Check if the file is not empty
     if filename == '':
         return jsonify({'error': 'No selected file'})
+    filename = filename.replace(" ", "").replace(',', '').replace('_', '').replace("'", "")
+    print("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",filename)
+    # Split from the last dot
+    file_extension = filename.rsplit('.', 1)[1]
+
+
+
+    
+    
+    
+
     
 
 
@@ -130,20 +153,73 @@ def make_file_index():
     #     response = {"message": f"Request error: {e}"}
     #     return jsonify([response])
     if es.indices.exists(index=filename):
-        print("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
         response = {"message": f"Index '{filename}' already exists."}
         return jsonify([response])
+
+    if request.form.get('file_format'):
+        file_format = request.form.get('file_format')
     
+        if file_format  in FILE_TO_CONVERT and file_format == 'csv':
+            # Process the file and convert to JSON
+            json_data = convert_csv_to_json(file)
+            # Save JSON data to a file
+
+            filename = filename.rsplit('.', 1)[0]
+
+            filename = filename+'.json'
+
+            save_json_to_file(json_data, './datastore/' + filename)
+            json_file_path = './datastore/'+filename
+
+        elif file_format in FILE_TO_CONVERT and file_format == 'xlsx':
+            # Process the file and convert to JSON
+            json_data = convert_excel_to_json(file)
+            print("json_data json_data", json_data)
+            # Save JSON data to a file
+
+            filename = filename.rsplit('.', 1)[0]
+
+            filename = filename+'.json'
+            print('./datastore/' + filename)
+
+            save_json_to_file(json_data, './datastore/' + filename)
+            json_file_path = './datastore/'+filename
+
+            # # CSV file path
+            # csv_file_path = '../datastore/Telegram/linkedin_united kingdom_0.csv'
+
+            # # JSON file path (where you want to save the converted JSON data)
+            # json_file_path = '../json/linkedin_united kingdom_0.json'
+
+            
+
+            # Open the CSV file for reading
+            # with open(csv_file_path, 'r') as csv_file:
+            #     # Create a CSV reader
+            #     csv_reader = csv.DictReader(csv_file)
+                
+            #     # Convert CSV data to a list of dictionaries
+            #     data = [row for row in csv_reader if any(row.values())]
+            #     print("data", data)
+
+            # # Write the JSON data to a file
+            # with open(json_file_path, 'w') as json_file:
+            #     # Use the json module to dump the data to JSON format
+            #     json.dump(data, json_file, indent=2)
+        
 
     # Save the uploaded file to a designated folder
-    file.save('./datastore/' + filename)
-    print('./datastore/' + filename)
-    print("file successfully saved")
-    json_file_path = './datastore/'+filename
+    # file.save('./datastore/' + filename)
+    # print('./datastore/' + filename)
+    # print("file successfully saved")
+    else:
+        json_file_path = './datastore/'+filename
+        print("json_file_path", json_file_path)
+        # Save the uploaded file to a designated folder
+        file.save(json_file_path)
     # Check if the index already filename
     
     # Indexing JSON data
-    print("ooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
     try:
         with open(json_file_path, 'r') as json_file:
             data = json.load(json_file)
@@ -162,15 +238,14 @@ def make_file_index():
             try:
                 # Use the helpers.bulk() method for bulk indexing
                 success, failed = helpers.bulk(es, actions,index=filename, raise_on_error=True)
-                print(f"Successfully indexed: {success} documents")
-                print(f"Failed to index: {failed} documents")
+
                 response = {"message": f"Successfully indexed: {success} documents"}
                 # Print the details of failed documents
                 for idx, document in enumerate(data):
                     # success, failed = helpers.bulk(es, [document], raise_on_error=False)
                     if failed:
                         print(f"Failed to index document at index {idx}: {failed[0]['index']['error']}")
-                print("999999999999999999999999999999999999999999999999999")
+
                 return jsonify([response])
                 
                 # if failed:
@@ -179,15 +254,46 @@ def make_file_index():
                 #         return jsonify([response])
                         
             except Exception as e:
-                print(f"Error during bulk indexing: {e}")
                 response = {"message": f"Error during bulk indexing: {e}"}
                 # Print the failed documents for more details
-                print("data")
 
                 return jsonify([response])
     except Exception as e:
 
         return str(e)
+    
+
+def convert_csv_to_json(file):
+    # Use StringIO to treat the file as a string
+    with StringIO(file.read().decode('utf-8')) as csv_file:
+        # Create a CSV reader
+        csv_reader = csv.DictReader(csv_file)
+
+        # Convert CSV to JSON
+        json_data = json.dumps(list(csv_reader), indent=2)
+
+    return json_data
+
+def convert_excel_to_json(excel_file):
+    try:
+        # Read Excel file into DataFrame
+        excel_data_df = pd.read_excel(BytesIO(excel_file.read()))
+
+        # Convert DataFrame to JSON string
+        json_str = excel_data_df.to_json(orient='records')
+
+        # Save the JSON string to a file or do further processing
+        # For example, you can return the JSON string to the frontend
+
+        print('Conversion Successful from Excel to JSON!')
+        return json_str
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
+
+def save_json_to_file(json_data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(json.loads(json_data), json_file, indent=2)
 
 
      
